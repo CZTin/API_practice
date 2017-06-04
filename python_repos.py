7ec76eac70c2b723c4f317c2392e018ad6387c1c
ZincS <874598493@qq.com>
2017-06-04 14:07:15 +0800

    17.2.4 add url function

--- python_repos.py
+++ python_repos.py
@@ -22,12 +22,14 @@ for repo_dict in repo_dicts:
 	if repo_dict['description'] == None:
 		plot_dict = {
 			'value': repo_dict['stargazers_count'], 
-			'label': 'hahaha'
+			'label': 'No information.',
+			'xlink': repo_dict['html_url'],
 			}
 	else:
 		plot_dict = {
 			'value': repo_dict['stargazers_count'], 
-			'label': repo_dict['description']
+			'label': repo_dict['description'],
+			'xlink': repo_dict['html_url'],
 			}
 
 	plot_dicts.append(plot_dict)
