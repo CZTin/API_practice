025646afd32034f327bf3f59c26410298475e08b
ZincS <874598493@qq.com>
2017-06-04 12:32:34 +0800

    17.2 chart in pygal

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 #执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort-stars'
@@ -11,21 +13,17 @@ print("Total repositories:", response_dict['total_count'])
 
 #探索有关仓库的信息
 repo_dicts = response_dict['items']
-print("Respositories returned:", len(repo_dicts))
 
-#研究第一个仓库
-#repo_dict = repo_dicts[0]
-#print("\nKeys:", len(repo_dict))
-#for key in sorted(repo_dict.keys()):
-#	print(key)
-
-#处理结果
-#print(response_dict.keys())
-
-print("\nSelected information about first repository:")
-
+names, stars = [], []
 for repo_dict in repo_dicts:
-	print('\nName:', repo_dict['name'])
-	print('Owner:', repo_dict['owner']['login'])
-	print('Stars:', repo_dict['stargazers_count'])
-	print('Respository', repo_dict['html_url'])
+	names.append(repo_dict['name'])
+	stars.append(repo_dict['stargazers_count'])
+
+#可视化
+my_style = LS('#336699', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+
+chart.add('',stars)
+chart.render_to_file('python_repos.svg')
